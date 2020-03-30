43848a0987c01f7479cda5b327da2291a715f037
jasontan056 <jasontan056@gmail.com>
2020-03-29 20:46:16 -0700

    Embed a youtube player

--- src/Room.js
+++ src/Room.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useParams } from "react-router-dom";
+import YouTube from "react-youtube";
 
 export default function Room() {
   const { roomId } = useParams();
+  const [youtubeTarget, setYoutubeTarget] = useState(null);
 
-  return <p>In room page for {roomId}</p>;
+  const youtubePlayerOpts = {
+    height: "576",
+    width: "1024",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const onReady = (event) => {
+    setYoutubeTarget(event.target);
+  };
+
+  const videoId = "qQjMMEWaWsc";
+  return (
+    <div>
+      <p>In room page for {roomId}</p>
+      <YouTube videoId={videoId} opts={youtubePlayerOpts} onReady={onReady} />
+    </div>
+  );
 }
