20872e26bc6cb731daf30ec4c54cddb026854cff
jasontan056 <jasontan056@gmail.com>
2020-04-01 16:45:53 -0700

    Add basic actions

---
+++ src/actions.js
@@ -0,0 +1,33 @@
+/*
+ * Action types
+ */
+
+export const SET_PLAYBACK_STATE = "SET_PLAYBACK_STATE";
+export const SEEK_TO = "SEEK_TO";
+export const SET_VIDEO_ID = "SET_VIDEO_ID";
+
+/*
+ * Other constants
+ */
+
+export const PlaybackStates = {
+  PLAYING: "PLAYING",
+  PAUSED: "PAUSED",
+  BUFFERING: "BUFFERING"
+};
+
+/*
+ * Action creators
+ */
+
+export function setPlaybackState(playbackState) {
+  return { type: SET_PLAYBACK_STATE, playbackState };
+}
+
+export function seekTo(seekPositionSec) {
+  return { type: SEEK_TO, seekPositionSec };
+}
+
+export function setVideoId(videoId) {
+  return { type: SET_VIDEO_ID, videoId };
+}
