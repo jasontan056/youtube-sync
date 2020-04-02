bce89150fdcb69404625b28dd44ede8f82edce9b
jasontan056 <jasontan056@gmail.com>
2020-04-01 17:11:22 -0700

    Create a store for the syncingPlayerApp and try it out

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import CreateRoom from "./CreateRoom";
-import Room from "./Room";
+import SyncingPlayer from "./SyncingPlayer";
 
 function App() {
   return (
     <Router>
       <Switch>
         <Route path="/room/:roomId">
-          <Room />
+          <SyncingPlayer />
         </Route>
         <Route path="/">
           <CreateRoom />
