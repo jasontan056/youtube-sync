c8f2ea46571ff1402730544e96df409b7672e4ea
jasontan056 <jasontan056@gmail.com>
2020-04-01 17:12:24 -0700

    Remove code used to try out the syncingPlayerApp store

--- src/SyncingPlayer.js
+++ src/SyncingPlayer.js
@@ -1,29 +1,8 @@
 import React from "react";
 import { createStore } from "redux";
 import syncingPlayerApp from "./reducers";
-import {
-  PlaybackStates,
-  setPlaybackState,
-  seekTo,
-  setVideoId
-} from "./actions";
 
 const store = createStore(syncingPlayerApp);
-
-// Log the initial state
-console.log(store.getState());
-
-// Every time the state changes, log it
-// Note that subscribe() returns a function for unregistering the listener
-const unsubscribe = store.subscribe(() => console.log(store.getState()));
-
-// Dispatch some actions
-store.dispatch(setPlaybackState(PlaybackStates.PLAYING));
-store.dispatch(seekTo(50));
-store.dispatch(setVideoId("newVideoId"));
-
-// Stop listening to state updates
-unsubscribe();
 
 export default function SyncingPlayer() {
   return <p>syncing player</p>;
