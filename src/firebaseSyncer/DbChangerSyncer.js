043f7f75a8b5f84a0f20da7c13716ce706a126db
jasontan056 <jasontan056@gmail.com>
2020-04-03 23:13:22 -0700

    Subscribe to room state in firebase DB and dispatch actions on room state from DbChangerSyncer

--- src/firebaseSyncer/DbChangerSyncer.js
+++ src/firebaseSyncer/DbChangerSyncer.js
@@ -2,25 +2,89 @@ import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { roomActionCreators, PlaybackStates } from "../actions";
+import Firebase from "../Firebase";
 
 export const DbChangerSyncer = ({
   roomId,
+  onVideoIdChange,
   onPlaybackStateChange,
   onSeekChange,
-  onVideoIdChange,
   onPlayStartTimestampChange,
   onUserBufferingAdded,
   onUserBufferingRemoved,
 }) => {
-  useEffect(() => {}, [roomId]);
+  useEffect(() => {
+    const roomRef = Firebase.database().ref(`room/${roomId}`);
+    const videoIdRef = roomRef.child("videoId");
+    videoIdRef.on("value", (snapshot) => {
+      const videoId = snapshot.val();
+      if (!videoId) {
+        return;
+      }
+      onVideoIdChange(videoId);
+    });
+
+    const playbackStateRef = roomRef.child("playbackState");
+    playbackStateRef.on("value", (snapshot) => {
+      const playbackState = snapshot.val();
+      if (!playbackState) {
+        return;
+      }
+      onPlaybackStateChange(playbackState);
+    });
+
+    const seekPositionRef = roomRef.child("seekPosition");
+    seekPositionRef.on("value", (snapshot) => {
+      const seekPosition = snapshot.val();
+      if (!seekPosition) {
+        return;
+      }
+      onSeekChange(seekPosition);
+    });
+
+    const playStartTimestampRef = roomRef.child("playStartTimestamp");
+    playStartTimestampRef.on("value", (snapshot) => {
+      const playStartTimestamp = snapshot.val();
+      if (!playStartTimestamp) {
+        return;
+      }
+      onPlayStartTimestampChange(playStartTimestamp);
+    });
+
+    const usersBufferingRef = roomRef.child("usersBuffering");
+    usersBufferingRef.on("child_added", (data) => {
+      const userId = data.key;
+      onUserBufferingAdded(userId);
+    });
+    usersBufferingRef.on("child_removed", (data) => {
+      const userId = data.key;
+      onUserBufferingRemoved(userId);
+    });
+
+    return function cleanup() {
+      videoIdRef.off();
+      playbackStateRef.off();
+      seekPositionRef.off();
+      playStartTimestampRef.off();
+      usersBufferingRef.off();
+    };
+  }, [
+    onPlayStartTimestampChange,
+    onPlaybackStateChange,
+    onSeekChange,
+    onUserBufferingAdded,
+    onUserBufferingRemoved,
+    onVideoIdChange,
+    roomId,
+  ]);
   return <div>dbChangeSyncer</div>;
 };
 
 DbChangerSyncer.propTypes = {
   roomId: PropTypes.string.isRequired,
+  onVideoIdChange: PropTypes.func.isRequired,
   onPlaybackStateChange: PropTypes.func.isRequired,
   onSeekChange: PropTypes.func.isRequired,
-  onVideoIdChange: PropTypes.func.isRequired,
   onPlayStartTimestampChange: PropTypes.func.isRequired,
   onUserBufferingAdded: PropTypes.func.isRequired,
   onUserBufferingRemoved: PropTypes.func.isRequired,
@@ -31,12 +95,12 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
+  onVideoIdChange: (videoId) =>
+    dispatch(roomActionCreators.setVideoId(videoId)),
   onPlaybackStateChange: (state) =>
     dispatch(roomActionCreators.setPlaybackState(state)),
   onSeekChange: (seekPosition) =>
     dispatch(roomActionCreators.seekTo(seekPosition)),
-  onVideoIdChange: (videoId) =>
-    dispatch(roomActionCreators.setVideoId(videoId)),
   onPlayStartTimestampChange: (playStartTimestamp) =>
     dispatch(roomActionCreators.setPlayStartTimestamp(playStartTimestamp)),
   onUserBufferingAdded: (userId) =>
